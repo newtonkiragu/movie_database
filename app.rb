c91468c55b6b6fc2c783b3d16e3cd17576f85d15
newtonkiragu <karanunewton4@gmail.com>
2017-07-12 12:23:13 +0300

    added the post and get methods for the actors and movies page

--- app.rb
+++ app.rb
@@ -20,3 +20,31 @@ get("/movies") do
   @movies = Movie.all()
   erb(:movies)
 end
+
+post("/actors") do
+  name = params.fetch("name")
+  actor = Actor.new({:name => name, :id => nil})
+  actor.save()
+  @actors = Actor.all()
+  erb(:actors)
+end
+
+post("/movies") do
+  name = params.fetch("name")
+  movie = Movie.new({:name => name, :id => nil})
+  movie.save()
+  @movies = Movie.all()
+  erb(:movies)
+end
+
+get("/actors/:id") do
+  @actor = Actor.find(params.fetch("id").to_i())
+  @movies = Movie.all()
+  erb(:actor_info)
+end
+
+get("/movies/:id") do
+  @movie = Movie.find(params.fetch("id").to_i())
+  @actors = Actor.all()
+  erb(:movie_info)
+end
