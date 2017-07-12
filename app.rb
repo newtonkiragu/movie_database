fcb7db88e2e5005855580f8cf8e058a82c9462c5
newtonkiragu <karanunewton4@gmail.com>
2017-07-12 12:20:25 +0300

    added index route to app file

---
+++ app.rb
@@ -0,0 +1,22 @@
+require("sinatra")
+require("sinatra/reloader")
+also_reload("lib/**/*.rb")
+require("./lib/actor")
+require("./lib/movie")
+require("pg")
+
+get("/") do
+  @actors = Actor.all()
+  @movies = Movie.all()
+  erb(:index)
+end
+
+get("/actors") do
+  @actors = Actor.all()
+  erb(:actors)
+end
+
+get("/movies") do
+  @movies = Movie.all()
+  erb(:movies)
+end
