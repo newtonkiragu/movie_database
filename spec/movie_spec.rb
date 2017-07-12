48f735dc560e3081c31e8f9b93976e0aedaba2f0
newtonkiragu <karanunewton4@gmail.com>
2017-07-12 11:52:12 +0300

    added a new spec in movies to ascociate actors with movies

--- spec/movie_spec.rb
+++ spec/movie_spec.rb
@@ -33,6 +33,17 @@ describe(Movie) do
       movie.update({:name => "Oceans Twelve"})
       expect(movie.name()).to(eq("Oceans Twelve"))
     end
+
+    it("lets you add an actor to a movie") do
+      movie = Movie.new({:name => "Oceans Eleven", :id => nil})
+      movie.save()
+      george = Actor.new({:name => "George Clooney", :id => nil})
+      george.save()
+      brad = Actor.new({:name => "Brad Pitt", :id => nil})
+      brad.save()
+      movie.update({:actor_ids => [george.id(), brad.id()]})
+      expect(movie.actors()).to(eq([george, brad]))
+    end
   end
 
   describe("#delete") do
@@ -44,5 +55,19 @@ describe(Movie) do
       movie.delete()
       expect(Movie.all()).to(eq([movie2]))
     end
+
+    describe("#actors") do
+      it("returns all of the actors in a particular movie") do
+        movie = Movie.new({:name => "Oceans Eleven", :id => nil})
+        movie.save()
+        george = Actor.new({:name => "George Clooney", :id => nil})
+        george.save()
+        brad = Actor.new({:name => "Brad Pitt", :id => nil})
+        brad.save()
+        movie.update({:actor_ids => [george.id(), brad.id()]})
+        expect(movie.actors()).to(eq([george, brad]))
+      end
+    end
+
   end
 end
